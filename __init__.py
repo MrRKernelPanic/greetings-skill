3436e8b32d35b532c671345349943b6b4b030e3d
Mark Routledge <mark@mroutledge.co.uk>
2020-08-28 23:45:09 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,14 +1,21 @@
-from mycroft import MycroftSkill, intent_file_handler
+from adapt.intent import IntentBuilder
+from mycroft.skills.core import MycroftSkill
 
-
-class Greetings(MycroftSkill):
+class GreetingsSkill(MycroftSkill):
+    
     def __init__(self):
-        MycroftSkill.__init__(self)
+        super(GreetingsSkill, self).__init__("GreetingsSkill")
+    
+    def initialize(self):
+        greetings_intent= IntentBuilder("GreetingsIntent"). \
+            require("GreetingsKeyword").build()
+        self.register_intent(greetings_intent, self.handle_greetings_intent)
+        
+    def handle_greetings_intent(self, message):
+        self.speak_dialog("greetings")
 
-    @intent_file_handler('greetings.intent')
-    def handle_greetings(self, message):
-        self.speak_dialog('greetings')
-
+    def stop(self):
+        pass
 
 def create_skill():
     return Greetings()
