2ffc515a6db312f411bf42179d132ee09beefa6d
Mark Routledge <mark@mroutledge.co.uk>
2020-08-29 00:03:55 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -7,16 +7,27 @@ class GreetingsSkill(MycroftSkill):
         super(GreetingsSkill, self).__init__("GreetingsSkill")
     
     def initialize(self):
-        greetings_intent= IntentBuilder("GreetingsIntent"). \
-            require("GreetingsKeyword").build()
-        self.register_intent(greetings_intent, self.handle_greetings_intent)
+        #For each command you need an intent and register, these 2 lines are for hello.
+        #This then call the 'def handler' that gives the response.  For each intent there should be a generated .VOC file that has the keyword.
+        #The .voc file should have the same name as the required "", e.g. "HelloKeyword" below
         
-    def handle_greetings_intent(self, message):
-        self.speak_dialog("greetings")
-
+        hello_intent= IntentBuilder("HelloIntent").require("HelloKeyword").build()
+        self.register_intent(hello_intent, self.handle_hello_intent)
+        
+        good_evening_intent = IntentBuilder("GoodEveningIntent".require("GoodEveningKeyword".build()
+        self.register_intent(good_evening_intent, self.handle_good_evening_intent)
+    
+    
+    
+    #For each of the above intents there is a response.
+    def handle_hello_intent(self, message):
+        self.speak_dialog("hello")
+                                                                        
+    def handle_good_evening(self, message):
+        self.speak_dialog("good evening")
+        
     def stop(self):
         pass
 
 def create_skill():
     return Greetings()
-
